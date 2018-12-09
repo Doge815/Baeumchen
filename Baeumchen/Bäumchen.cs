0042050e8a8b0d46926aed3bf28317b430ba2a12
Doge815 <42283470+Doge815@users.noreply.github.com>
2018-12-09 12:13:57 +0100

    start Kommentare

--- Baeumchen/Bäumchen.cs
+++ Baeumchen/Bäumchen.cs
@@ -11,48 +11,48 @@ using System.Windows.Forms;
 
 namespace Baeumchen
 {
-    public class Bäumchen
+    public class Bäumchen   //Die Baum-Hauptklasse
     {
-        private readonly int my_int;
-        private List<Bäumchen> sons = new List<Bäumchen> { null, null };
-        internal Bäumchen geschlechtslosesElternteil = null;
+        private readonly int my_int;    //Eigener Wert
+        private List<Bäumchen> sons = new List<Bäumchen> { null, null };    //Jerder Baum KANN 2 Kinder haben
+        internal Bäumchen geschlechtslosesElternteil = null;    //aka Vater
         private Baummaler my_maler = null;
 
         public int Value { get { return my_int; } }
         public List<Bäumchen> Sons {  get { return sons; } }
 
         public Bäumchen(int value) => my_int = value;
-        internal Bäumchen(int value, Bäumchen father)
+        internal Bäumchen(int value, Bäumchen father)   //Konstruktor der nur genutzt wird, um Kinder zu erzeugen, aber nicht den Haupt-Baum 
         {
             my_int = value;
             geschlechtslosesElternteil = father;
         }
 
 
-        public void Add(int value)
+        public void Add(int value)  //Fügt neue Werte hinzu
         {
-            if (value == my_int) goto Skip;
+            if (value == my_int) goto Skip; //Wenn der Wert gleich dem eigenen ist, wird geskipt
             int pos = (value < my_int) ? (0) : (1);
-            if (sons[pos] == null) sons[pos] = new Bäumchen(value, this);
-            else sons[pos].Add(value);
+            if (sons[pos] == null) sons[pos] = new Bäumchen(value, this);   //Wenn kein Sohn an der Position existiert, wird ein neuer erstellt 
+            else sons[pos].Add(value);  //wenn einer existiert, wird der Wert weitergegeben
             if (my_maler != null) my_maler.UpdateImage();
             Skip:;
         }
 
-        public List<int> Deep()
+        public List<int> Deep() //Gibt alle Elemente von sich Söhnen, deren Söhnen, usw. aus.
         {
             List<int> output = new List<int>();
-            if (sons[0] != null) output.AddRange(sons[0].Deep());
-            output.Add(my_int);
+            if (sons[0] != null) output.AddRange(sons[0].Deep());   //wenn ein Sohn existiert, rekursive Ausführung
+            output.Add(my_int); //eigenen Wert hinzufügeb
             if (sons[1] != null) output.AddRange(sons[1].Deep());
             return output;
         }
 
-        internal int GetDeep()
+        internal int GetDeep()  //Gibt die Tiefe des Baumes aus
         {
-            int deeper = (sons[0] != null) ? (sons[0].GetDeep()) : (0);
+            int deeper = (sons[0] != null) ? (sons[0].GetDeep()) : (0); //Tiefe vom linken Ast
             deeper = (sons[1] != null) ? ((sons[1].GetDeep()> deeper) ? (sons[1].GetDeep()) : ( deeper)) : (deeper);
-            return deeper + 1;
+            return deeper + 1;  // +1 für die eigene Tiefe
         }
 
         internal void GetMaler(Baummaler maler) => my_maler = maler;
